0f5653ff9bed88698dd2a41bbaf7a72f811335dd
Gleb <dawgkek@mail.ru>
2025-08-09 11:22:59 +0300

    added send files to kafka from dir messages

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-	"fmt"
+	"os"
+	"path/filepath"
 
 	k "github.com/AugustSerenity/order-service/internal/kafka"
 	"github.com/sirupsen/logrus"
 )
 
 const (
-	topic = "order"
+	topic       = "order"
+	messagePath = "/Users/glebbelov/WB/L0/order-service/cmd/producer/messages"
 )
 
 var addres = []string{"localhost:9093"}
@@ -18,13 +20,32 @@ func main() {
 	if err != nil {
 		logrus.Fatal(err)
 	}
+	defer p.Close()
 
-	for i := 0; i < 100; i++ {
-		msg := fmt.Sprintf("Kafka message %d", i)
-
-		if err := p.Produce(msg, topic); err != nil {
-			logrus.Error(err)
-		}
+	files, err := os.ReadDir(messagePath)
+	if err != nil {
+		logrus.Fatalf("failed to read message directory: %v", err)
 	}
 
+	for _, file := range files {
+		if file.IsDir() || filepath.Ext(file.Name()) != ".json" {
+			continue
+		}
+
+		fullPath := filepath.Join(messagePath, file.Name())
+
+		content, err := os.ReadFile(fullPath)
+		if err != nil {
+			logrus.Errorf("failed to read file %s: %v", fullPath, err)
+			continue
+		}
+
+		logrus.Infof("Sending file: %s", file.Name())
+
+		if err := p.Produce(string(content), topic); err != nil {
+			logrus.Errorf("failed to produce message from %s: %v", file.Name(), err)
+		} else {
+			logrus.Infof("Successfully sent message from %s", file.Name())
+		}
+	}
 }
